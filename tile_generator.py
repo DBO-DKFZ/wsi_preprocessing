657837c7f8710a2fe7c81d359b866d2c641f69b6
unknown <martin.hetz@kit.edu>
2022-02-01 15:48:27 +0100

    Added multiprocessing

--- tile_generator.py
+++ tile_generator.py
@@ -4,26 +4,24 @@ import cv2
 import os
 import json
 import sys
+import multiprocessing
 
 import xml.etree.ElementTree as ET
 import numpy as np
 import matplotlib.pyplot as plt
 
-from tqdm import tqdm
 from PIL import Image
-from xml.dom import minidom
 
 # Fix to get the dlls to load properly under python >= 3.8 and windows
 script_dir = os.path.dirname(os.path.realpath(__file__))
 try:
     openslide_dll_path = os.path.join(script_dir, "..", "openslide-win64-20171122", "bin")
     os.add_dll_directory(openslide_dll_path)
-    print(openslide_dll_path)
+    #print(openslide_dll_path)
 except Exception as e:
-    print(e)
+    pass
 
 import openslide
-
 
 class WSIHandler:
 
@@ -33,6 +31,7 @@ class WSIHandler:
         self.total_height = 0
         self.levels = 0
         self.current_level = 0
+        self.annotation_list = None
         try:
             self.config = self.load_config(config_path)
         except:
@@ -199,137 +198,123 @@ class WSIHandler:
         scaling_factor = int(self.slide.level_downsamples[level])
         total_tiles = len(tile_dict)
         tile_nb = 0
-        with tqdm(total=total_tiles, desc="Processing Tiles", bar_format="{l_bar}{bar} [ time left: {remaining} ]") as pbar:
 
-            for tile_key in tile_dict:
-                tile_x = tile_dict[tile_key]["x"] * scaling_factor
-                tile_y = tile_dict[tile_key]["y"] * scaling_factor
-                tile_size = tile_dict[tile_key]["size"] * scaling_factor
+        for tile_key in tile_dict:
+            tile_x = tile_dict[tile_key]["x"] * scaling_factor
+            tile_y = tile_dict[tile_key]["y"] * scaling_factor
+            tile_size = tile_dict[tile_key]["size"] * scaling_factor
 
-                patch_dict.update({tile_nb: {"x_pos": tile_x, "y_pos": tile_y, "size": tile_size, "patches": {}}})
+            patch_dict.update({tile_nb: {"x_pos": tile_x, "y_pos": tile_y, "size": tile_size, "patches": {}}})
 
-                # ToDo: rows and cols arent calculated correctly, instead a quick fix by using breaks was applied
-                rows = int(np.ceil((tile_size + overlap) / (patch_size - px_overlap)))
-                cols = int(np.ceil((tile_size + overlap) / (patch_size - px_overlap)))
+            # ToDo: rows and cols arent calculated correctly, instead a quick fix by using breaks was applied
+            rows = int(np.ceil((tile_size + overlap) / (patch_size - px_overlap)))
+            cols = int(np.ceil((tile_size + overlap) / (patch_size - px_overlap)))
 
-                tile = np.array(self.slide.read_region((tile_x, tile_y), level=0, size=(tile_size, tile_size)))
-                tile = tile[:, :, 0:3]
+            tile = np.array(self.slide.read_region((tile_x, tile_y), level=0, size=(tile_size, tile_size)))
+            tile = tile[:, :, 0:3]
 
-                # Translate from world coordinates to tile coordinates
-                tile_annotation_list = [[[point[0] - tile_x, point[1] - tile_y] for point in annotations[polygon]] for
-                                        polygon in annotations]
+            # Translate from world coordinates to tile coordinates
+            tile_annotation_list = [[[point[0] - tile_x, point[1] - tile_y] for point in annotations[polygon]] for
+                                    polygon in annotations]
 
-                # Create mask from polygons
-                tile_annotation_mask = np.zeros(shape=(tile_size, tile_size))
-                for polygon in tile_annotation_list:
-                    cv2.fillPoly(tile_annotation_mask, [np.array(polygon).astype(np.int32)], 1)
+            # Create mask from polygons
+            tile_annotation_mask = np.zeros(shape=(tile_size, tile_size))
+            for polygon in tile_annotation_list:
+                cv2.fillPoly(tile_annotation_mask, [np.array(polygon).astype(np.int32)], 1)
 
-                patch_nb = 0
-                stop_y = False
+            patch_nb = 0
+            stop_y = False
 
-                for row in range(rows):
-                    stop_x = False
+            for row in range(rows):
+                stop_x = False
 
-                    for col in range(cols):
+                for col in range(cols):
 
-                        # Calculate patch coordinates
-                        patch_x = int(col * (patch_size - px_overlap))
-                        patch_y = int(row * (patch_size - px_overlap))
+                    # Calculate patch coordinates
+                    patch_x = int(col * (patch_size - px_overlap))
+                    patch_y = int(row * (patch_size - px_overlap))
 
-                        if patch_y + patch_size >= tile_size:
-                            stop_y = True
-                            patch_y = tile_size - patch_size
+                    if patch_y + patch_size >= tile_size:
+                        stop_y = True
+                        patch_y = tile_size - patch_size
 
-                        if patch_x + patch_size >= tile_size:
-                            stop_x = True
-                            patch_x = tile_size - patch_size
+                    if patch_x + patch_size >= tile_size:
+                        stop_x = True
+                        patch_x = tile_size - patch_size
 
-                        patch = tile[patch_y:patch_y + patch_size, patch_x:patch_x + patch_size, :]
-                        patch_mask = tile_annotation_mask[patch_y:patch_y + patch_size, patch_x:patch_x + patch_size]
+                    patch = tile[patch_y:patch_y + patch_size, patch_x:patch_x + patch_size, :]
+                    patch_mask = tile_annotation_mask[patch_y:patch_y + patch_size, patch_x:patch_x + patch_size]
 
-                        label = self.check_for_label(label_dict, patch_mask)
+                    label = self.check_for_label(label_dict, patch_mask)
 
-                        if label is not None:
+                    if label is not None:
 
-                            patch_dict[tile_nb]["patches"].update(
-                                {patch_nb: {"x_pos": patch_x, "y_pos": patch_y, "patch_size": patch_size,
-                                            "label": label, "slide_name":slide_name}})
+                        patch_dict[tile_nb]["patches"].update(
+                            {patch_nb: {"x_pos": patch_x, "y_pos": patch_y, "patch_size": patch_size,
+                                        "label": label, "slide_name":slide_name}})
 
-                            if file_dir is not None:
-                                file_path = os.path.join(file_dir, label)
-                                if not os.path.exists(file_path):
-                                    os.makedirs(file_path)
+                        if file_dir is not None:
+                            file_path = os.path.join(file_dir, label)
+                            if not os.path.exists(file_path):
+                                os.makedirs(file_path)
 
-                                if slide_name is not None:
-                                    file_name = slide_name+"_"+str(tile_nb) + "_" + str(patch_nb) + ".png"
-                                else:
-                                    file_name = str(tile_nb) + "_" + str(patch_nb) + ".png"
-                                patch = Image.fromarray(patch)
-                                patch.save(os.path.join(file_path, file_name), format="png")
+                            if slide_name is not None:
+                                file_name = slide_name+"_"+str(tile_nb) + "_" + str(patch_nb) + ".png"
+                            else:
+                                file_name = str(tile_nb) + "_" + str(patch_nb) + ".png"
+                            patch = Image.fromarray(patch)
+                            patch.save(os.path.join(file_path, file_name), format="png")
 
-                            patch_nb += 1
-                        if stop_x:
-                            break
-                    if stop_y:
+                        patch_nb += 1
+                    if stop_x:
                         break
+                if stop_y:
+                    break
 
-                tile_nb += 1
-                pbar.update(1)
+            tile_nb += 1
 
         return patch_dict
 
-    def stain_test(self):
-        img, lvl = self.get_img(6, show=True)
-        img = img[:, :, 0:3]
+    def process_slide(self, slide):
 
-        I, H, E = tissue_detection.extract_stains(img)
+        slide_name = os.path.splitext(slide)[0]
+        if not (slide_name in self.annotation_list) and self.config["skip_unlabeled_slides"]:
+            print("Skipping slide", slide_name, "- No annotation found")
+        else:
+            print("Found annotation for slide", slide_name, "process id is",os.getpid())
 
-        plt.imshow(H)
-        plt.title("H stain")
-        plt.show()
-        plt.imshow(E)
-        plt.title("E Stain")
-        plt.show()
-        plt.imshow(I)
-        plt.title("Normalized")
-        plt.show()
+            annotation_path = os.path.join(self.config["annotation_dir"],
+                                           slide_name + "." + self.config["annotation_file_format"])
+            annotation_dict = self.load_annotation(annotation_path)
+            slide_path = os.path.join(self.config["slides_dir"], slide)
+            self.load_slide(slide_path)
+            mask, level = self.apply_tissue_detection(level=self.config["processing_level"],
+                                                               show=self.config["show_mode"])
+
+            tile_dict = self.get_relevant_tiles(mask, tile_size=self.config["tile_size"],
+                                                                     min_coverage=self.config["tissue_coverage"],
+                                                                     level=level,
+                                                                     show=self.config["show_mode"])
+
+            patch_dict = self.extract_patches(tile_dict,
+                                                       level,
+                                                       annotation_dict,
+                                                       self.config["label_dict"],
+                                                       file_dir=self.config["output_path"],
+                                                       overlap=self.config["overlap"],
+                                                       patch_size=self.config["patch_size"],
+                                                       slide_name=slide_name)
+
+            self.save_patch_configuration(patch_dict, slide_name)
+
+            print("Finished Slide", slide)
 
     def slides2patches(self):
-
         slide_list = os.listdir(self.config["slides_dir"])
         annotation_list = os.listdir(self.config["annotation_dir"])
-        annotation_list = [os.path.splitext(annotation)[0] for annotation in annotation_list]
-
-        for slide in slide_list:
-
-            slide_name = os.path.splitext(slide)[0]
-            if not (slide_name in annotation_list) and self.config["skip_unlabeled_slides"]:
-                print("Skipping slide", slide_name)
-            else:
-                print("Found annotation for slide", slide_name)
-                annotation_path = os.path.join(self.config["annotation_dir"],
-                                               slide_name + "." + self.config["annotation_file_format"])
-                annotation_dict = self.load_annotation(annotation_path)
-                slide_path = os.path.join(self.config["slides_dir"], slide)
-                self.load_slide(slide_path)
-                mask, level = slide_handler.apply_tissue_detection(level=self.config["processing_level"],
-                                                                   show=self.config["show_mode"])
-
-                tile_dict = slide_handler.get_relevant_tiles(mask, tile_size=self.config["tile_size"],
-                                                             min_coverage=self.config["tissue_coverage"],
-                                                             level=level,
-                                                             show=self.config["show_mode"])
-
-                patch_dict = slide_handler.extract_patches(tile_dict,
-                                                           level,
-                                                           annotation_dict,
-                                                           self.config["label_dict"],
-                                                           file_dir=self.config["output_path"],
-                                                           overlap=self.config["overlap"],
-                                                           patch_size=self.config["patch_size"],
-                                                           slide_name=slide_name)
-
-                self.save_patch_configuration(patch_dict, slide_name)
+        self.annotation_list = [os.path.splitext(annotation)[0] for annotation in annotation_list]
+        pool = multiprocessing.Pool()
+        pool.map(self.process_slide, slide_list)
 
 
 if __name__ == "__main__":
